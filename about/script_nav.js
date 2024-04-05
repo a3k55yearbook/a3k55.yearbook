cb95e4572ff3b18f9effbb2fa49e1db5347b0187
nhansp <nhan.094.vn@gmail.com>
2024-04-06 02:35:51 +0700

    sleepy

---
+++ about/script_nav.js
@@ -0,0 +1,9 @@
+$(function() {
+  var url = window.location.href;
+  $(".nav a").each(function() {
+      if (url == (this.href)) {
+          $(this).closest("li").addClass("active");
+         $(this).closest("li").parent().parent().addClass("active");
+      }
+  });
+}); 
