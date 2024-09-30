ef1d7749de31ebd25a36b6556de71a7a4fb9420a
nhansp <nhan.094.vn@gmail.com>
2024-09-30 21:06:36 +0700

    script: cleanup and bug fixes
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

--- members/script_nav.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  var url = window.location.href;
-  $(".nav a").each(function() {
-      if (url == (this.href)) {
-          $(this).closest("li").addClass("active");
-         $(this).closest("li").parent().parent().addClass("active");
-      }
-  });
-}); 
